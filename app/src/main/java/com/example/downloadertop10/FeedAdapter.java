d5fe5734f7d53843b178fd04a099c6c84a691857
aryaus <ar.gholami.bl@gmail.com>
2024-11-11 21:24:43 +0100

    add viewHolder

--- app/src/main/java/com/example/downloadertop10/FeedAdapter.java
+++ app/src/main/java/com/example/downloadertop10/FeedAdapter.java
@@ -2,8 +2,12 @@ package com.example.downloadertop10;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -19,5 +23,48 @@ public class FeedAdapter extends ArrayAdapter {
         this.layoutInflater = LayoutInflater.from(context);
         this.applications = applications;
 
+    }
+
+    @Override
+    public int getCount() {
+        return applications.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(layoutResource, parent, false);
+
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+//        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+//        TextView tvArtist = (TextView) convertView.findViewById(R.id.tvArtist);
+//        TextView tvSummary = (TextView) convertView.findViewById(R.id.tvSummary);
+
+        FeedEntry currentApp = applications.get(position);
+
+        viewHolder.tvName.setText(currentApp.getName());
+        viewHolder.tvArtist.setText(currentApp.getArtist());
+        viewHolder.tvSummary.setText(currentApp.getSummary());
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        final TextView tvName;
+        final TextView tvArtist;
+        final TextView tvSummary;
+
+        ViewHolder(View v){
+            this.tvName = v.findViewById(R.id.tvName);
+            this.tvArtist = v.findViewById(R.id.tvArtist);
+            this.tvSummary = v.findViewById(R.id.tvSummary);
+        }
     }
 }
